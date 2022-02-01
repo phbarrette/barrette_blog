4dbb67e664c67d5737dd0567842b3b2c5eaf96b1
phbarrette <m230342@usna.edu>
2022-02-01 10:43:04 -0500

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
-  get "/articles", to: "articles#index"
+  root "articles#index"
+
+  resources :articles
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
